2ec149383ebf18aaf6a0101fef08969a444e758c
Denis <deno.strbak@gmail.com>
2021-04-15 15:26:37 +0200

    volley

--- app/src/main/java/com/example/mtaa_strbakd_hanzelt/Search.kt
+++ app/src/main/java/com/example/mtaa_strbakd_hanzelt/Search.kt
@@ -3,16 +3,24 @@ package com.example.mtaa_strbakd_hanzelt
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.PopupMenu
 import android.widget.TextView
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 
 class Search : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
+        getVolley()
 
         val imageButton: ImageButton = findViewById(R.id.back)
         imageButton.setOnClickListener {
@@ -54,6 +62,20 @@ class Search : AppCompatActivity() {
             popupMenu.show()
         }
     }
+    fun getVolley(){
+        // Instantiate the RequestQueue.
+        val textView: TextView = findViewById(R.id.textView5)
+        val queue = Volley.newRequestQueue(this)
+        val url: String = "http://192.168.2.33:8000/cities/"
 
-
+        // Request a string response from the provided URL.
+        val stringReq = StringRequest(Request.Method.GET, url,
+                Response.Listener<String> { response ->
+                    textView.text = "hovno"
+                },
+                Response.ErrorListener {
+                    textView.text = "that didn't work"
+                })
+        queue.add(stringReq)
+    }
 }
